3d0d7139310decf09b5f1cb9b06c260b8970d4da
Lucas Felix <lucacfelix@gmail.com>
2024-05-28 11:25:26 -0300

    tratamentos de erros

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -2,10 +2,12 @@ package com.fatec.student.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,26 +25,32 @@ public class StudentController {
 
     @GetMapping    
 
-    public List<Student> getStudents(){
-    return studentService.getStudents();
+    public ResponseEntity<List<Student>> getStudents(){
+        return ResponseEntity.ok(studentService.getStudents());
 }
 
     @GetMapping("{id}")
 
-    public Student getStudentById(@PathVariable int id){
-        return studentService.getStudentById(id);
+    public ResponseEntity<Student> getStudentById(@PathVariable int id){
+        return ResponseEntity.ok(studentService.getStudentById(id));
     }
 
     @DeleteMapping("{id}")
 
-    public void deleteStudentById(@PathVariable int id){
+    public ResponseEntity<Void> deleteStudentById(@PathVariable int id){
         this.studentService.deleteStudentById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
 
     public Student save(@RequestBody Student student){
         return this.studentService.save(student);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable int id, @RequestBody Student student){
+        this.studentService.update(id,student);
     }
     
 }
